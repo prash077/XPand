a7b29c8f13fb71666f9fb93795e625e7bd86f7dc
unknown <rprashasthimmanuel@gmail.com>
2025-03-28 10:04:48 +0530

    userModel created for login | authController - authentiaction, Next: userModel for Logout

--- Server/controller/authController.js
+++ Server/controller/authController.js
@@ -28,7 +28,45 @@ export const register = async (req,res)=>{
             maxAge: 7*24*60*60*1000,
         });
         
+        return res.json({status:true,message:"Sign Up successful"});
+
     } catch (error) {
         res.json({status:failed,meaasge:error.meaasge});
+    }
+}
+
+export const Login = async (req,res)=>{
+    const {email,password} = req.body;
+    if(!email || !password)
+    {
+        return res.json({status:failed, message:"Missing Details"});
+    }
+    
+    try {
+        
+        const user = await userModel.findOne({email});
+        if(!user)
+        {
+            return res.json({status:failed, message:"User Not Found"});
+        }
+        const isCorrectPassword = await bcrypt.compare(password, user.password);
+
+        if(!isCorrectPassword)
+        {
+            return res.json({status:failed,message:"Invalid credentials"});
+        }
+
+        const token = jwt.sign({id : user._id}, process.env.SECRET_KEY, {expiresIn:'7d'});
+        res.cookie('token',token,{
+            httpOnly : true,
+            secure : process.env.NODE_ENV === "production",
+            sameSite : process.env.NODE_ENV === "production" ? 'none':'strict',
+            maxAge: 7*24*60*60*1000,
+        });
+        
+        return res.json({status:true, message:"Login Successfull"});
+
+    } catch (error) {
+        return res.json({status:failed, message:error.message});
     }
 }
